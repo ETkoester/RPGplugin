af1dd44d7fd8d991e7fea24668e26cac32fa7d66
Eddie <eddieeddie279@gmail.com>
2023-09-20 23:42:28 +0800

    init repository

---
+++ src/main/java/com/minecraft/rpgplugin/RPGplugin.java
@@ -0,0 +1,17 @@
+package com.minecraft.rpgplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPGplugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
